f5634fdaea98f31b13f3138a81a8dab49b119b4e
raam <jeya.colombo@gmail.com>
2023-12-05 15:06:37 -0500

    Footer developed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div className='footerParent'> Hi Footer</div>
+        <div className='footerParent'>
+            <div className='footParImg'>
+                <img src="https://bcassetcdn.com/public/blog/wp-content/uploads/2019/07/18094848/kingfisher-2.png" alt="" />
+            </div>
+            <div className='footerMidd'>
+                <div className="footerMiddEle">Shopping</div>
+                <div className="footerMiddEle">Entertainment</div>
+                <div className="footerMiddEle">Books</div>
+            </div>
+            <div>
+                <img src="https://cdn-icons-png.flaticon.com/128/2111/2111463.png?ga=GA1.1.1561457823.1701698226" alt="" className="footerLogo" />
+                <img src="https://cdn-icons-png.flaticon.com/128/2504/2504792.png?ga=GA1.1.1561457823.1701698226" alt="" className="footerLogo" />
+                <img src="https://cdn-icons-png.flaticon.com/128/4494/4494477.png?ga=GA1.1.1561457823.1701698226" alt="" className="footerLogo" />
+                <img src="https://cdn-icons-png.flaticon.com/128/4494/4494487.png?ga=GA1.1.1561457823.1701698226" alt="" className="footerLogo" />
+            </div>
+        </div>
     )
 }
 
