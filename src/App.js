1976e76cb6a80a4ea35081e65dabfa6b559aaab9
raam <jeya.colombo@gmail.com>
2023-12-07 19:08:25 -0500

    Home router path changed

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <bevContext.Provider value={beverage}>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/Home' element={<Home />} />
           <Route path='/News' element={<News />} />
         </Routes>
         <Footer/>
